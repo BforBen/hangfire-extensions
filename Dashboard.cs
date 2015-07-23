7945f4b271a3e9a0881922502671a05c1980bc18
Ben <BforBen@users.noreply.github.com>
2015-07-23 12:32:38 +0100

    Initial commit

---
+++ Dashboard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangfire.Dashboard
+{
+    public class NoAuthFilter : IAuthorizationFilter
+    {
+        public bool Authorize(IDictionary<string, object> owinEnvironment)
+        {
+            return true;
+        }
+    }
+}
---
+++ JobAttributes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Hangfire.Common;
+using Hangfire.States;
+using Hangfire.Storage;
+
+namespace Hangfire.Attributes
+{
+    public class UseQueueFromParameterAttribute : JobFilterAttribute, IElectStateFilter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueueAttribute"/> class
+        /// using the specified queue name.
+        /// </summary>
+        /// <param name="queue">Queue name.</param>
+        public UseQueueFromParameterAttribute(int parameterIndex)
+        {
+            this.ParameterIndex = parameterIndex;
+        }
+
+        public int ParameterIndex { get; private set; }
+
+        public void OnStateElection(ElectStateContext context)
+        {
+            var enqueuedState = context.CandidateState as EnqueuedState;
+            if (enqueuedState != null)
+            {
+                enqueuedState.Queue = context.Job.Arguments[ParameterIndex].Replace("\"", string.Empty);
+            }
+        }
+    }
+
+    public class ProlongSuccessJobExpirationTimeAttribute : JobFilterAttribute, IApplyStateFilter
+    {
+        public void OnStateApplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
+        {
+            context.JobExpirationTimeout = TimeSpan.FromDays(35);
+        }
+
+        public void OnStateUnapplied(ApplyStateContext content, IWriteOnlyTransaction transcation) { }
+    }
+}
